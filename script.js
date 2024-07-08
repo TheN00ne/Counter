69c31a5369fca370603db6278aedf9fd3dc07e06
Noone <ilyind777@gmail.com>
2024-07-09 00:50:50 +0300

    rgb

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function increase(e){
 }
 
 counter.addEventListener("click", increase);
+
+const btn = document.getElementById("btn");
+const col = document.getElementById("col");
+
+btn.addEventListener("click", () => {
+    let r = Math.round(Math.random() * 255);
+    let g = Math.round(Math.random() * 255);
+    let b = Math.round(Math.random() * 255);
+
+    let rgb = `rgb(${r}, ${g}, ${b})`;
+
+    document.body.style.backgroundColor = rgb;
+    col.innerHTML= rgb;
+})
